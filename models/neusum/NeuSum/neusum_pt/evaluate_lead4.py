cbb81f08f6624cdc763b338a74cc6ceb4cd1a906
ypyelab <jenny.yelabello@epfl.ch>
2020-10-06 13:36:03 +0200

    lead4

--- models/neusum/NeuSum/neusum_pt/evaluate_lead4.py
+++ models/neusum/NeuSum/neusum_pt/evaluate_lead4.py
@@ -5,7 +5,7 @@ rouge = Rouge.Rouge()
 
 
 def main(src_file, tgt_file):
-    all_lead3 = []
+    all_lead4 = []
     all_ref = []
 
     with open(src_file, 'r', encoding='utf-8') as src_reader, \
@@ -15,13 +15,13 @@ def main(src_file, tgt_file):
             tgt_line = tgt_line.strip()
             src_sents = src_line.split("##SENT##")
             tgt_sents = tgt_line.split("##SENT##")
-            lead3 = src_sents[:3]
-            all_lead3.append(' '.join(lead3))
+            lead4 = src_sents[:4]
+            all_lead4.append(' '.join(lead4))
             all_ref.append(' '.join(tgt_sents))
             # all_lead3.append(lead3)
             # all_ref.append(tgt_sents)
 
-    score = rouge.compute_rouge(all_ref, all_lead3)
+    score = rouge.compute_rouge(all_ref, all_lead4)
     print(score)
 
 
